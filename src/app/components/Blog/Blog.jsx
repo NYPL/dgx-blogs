74b23eb7049188baee4cb96f30426afb779b56ff
Patricio Gabriel Maseda <pmaseda@devspark.com>
2016-04-20 15:36:06 -0300

    WEB-2519 PR fixes.

--- src/app/components/Blog/Blog.jsx
+++ src/app/components/Blog/Blog.jsx
@@ -8,36 +8,45 @@ class Blog extends React.Component {
     super(props);
   }
 
-  createMarkup(bodyText) { 
-    return {__html: bodyText}
+  createMarkup(bodyText) {
+    return { __html: bodyText };
   }
-  
+
   render() {
     const unescapedBody = this.createMarkup(this.props.body);
 
     return (
       <div className="blogContent">
-        <p className="blogContent-date">{this.props.date}</p>
-      	<h1 
-          className="blogContent-title">
-          {this.props.title}
-        </h1>
+        <p className="blogContent-date">{ this.props.date }</p>
+        <h1 className="blogContent-title">{ this.props.title }</h1>
         <div className="blogContent-author">
-          by <b>{this.props.author.fullName}</b>. {this.props.author.title}
+          by <b>{this.props.author.fullName}</b>. { this.props.author.title }
         </div>
-        <p className="blogContent-date">{this.props.date}</p>
-        <img className="blogContent-mainPicture" src={this.props.mainPicture} />
-        <div 
+        <p className="blogContent-date">{ this.props.date }</p>
+        <img className="blogContent-mainPicture" src={ this.props.mainPicture } />
+        <div
           className="blogContent-bodyText"
-          dangerouslySetInnerHTML={unescapedBody}>
+          dangerouslySetInnerHTML={ unescapedBody }
+        >
         </div>
       </div>
     );
   }
 }
 
+Blog.propTypes = {
+  date: React.PropTypes.string,
+  title: React.PropTypes.string,
+  body: React.PropTypes.string,
+  mainPicture: React.PropTypes.string,
+  author: React.PropTypes.shape({
+    fullName: React.PropTypes.string,
+    title: React.PropTypes.string,
+  }),
+};
+
 Blog.defaultProps = {
-  author: {}
+  author: {},
 };
 
 export default Blog;
