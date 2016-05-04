197147c293ae317b3143da8d90d6daff33a8825d
Patricio Gabriel Maseda <pmaseda@devspark.com>
2016-05-04 11:11:49 -0300

    WEB-2674 Portrait two patterns initial.

--- src/app/components/BlogListing/BlogListing.jsx
+++ src/app/components/BlogListing/BlogListing.jsx
@@ -20,21 +20,35 @@ class BlogListing extends React.Component {
     return null;
   }
 
+  _seriesTitle() {
+    if (this.props.series !== null && this.props.series[0] !== null) {
+      if (this.props.mainPicture && this.props.mainPicture['full-uri']) {
+        return (<p className="blogListing-series">{this.props.series[0].title}</p>);
+      } else {
+        return (<p className="blogListing-fullWidthSeries">{this.props.series[0].title}</p>);
+      }
+    } else { 
+      return null;
+    }
+  }
+
   render() {
-    const seriesTitle = (this.props.series !== null && this.props.series[0] !== null) ?
-      <p className="blogListing-series">{this.props.series[0].title}</p> : null;
 
     /* place the image on the left or right side randomly */
     const rightOrLeft = (Math.round(Math.random())) ? 'Right' : '';
 
     /* apply a different class to the text paragraph when there's no image to show */
     const paragraphClass = this.props.mainPicture && this.props.mainPicture['full-uri'] ?
-      'blogListing-paragraph' + rightOrLeft : 'blogListing-fullWidthParagraph';
+      `blogListing-paragraph${rightOrLeft}` : 'blogListing-fullWidthParagraph';
+
+    /* same for title */
+    const titleClass = this.props.mainPicture && this.props.mainPicture['full-uri'] ?
+      'blogListing-title' : 'blogListing-fullWidthTitle';
 
     return (
       <div className="blogListing">
-        {seriesTitle}
-        <h2 className="blogListing-title">
+        {this._seriesTitle()}
+        <h2 className={titleClass}>
           <a href={`/blog/${this.props.slug}`}>
             {this.props.title}
           </a>
