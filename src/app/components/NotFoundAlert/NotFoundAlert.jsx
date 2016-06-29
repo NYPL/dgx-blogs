9f57f818ca5fd96a52cc3d624be04c8bb1fe062e
Patricio Gabriel Maseda <pmaseda@devspark.com>
2016-06-29 15:28:12 -0300

    WEB-3056 Bug not existent blog breaking the app solved. Message of blog not existent added.

---
+++ src/app/components/NotFoundAlert/NotFoundAlert.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class NotFoundAlert extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <div className="notFoundAlert">
+        <div className="notFoundAlert-message">
+          {this.props.message} 
+          <a href="/blog">Go back to the home page</a>
+        </div>
+      </div>
+    );
+  }
+}
+
+NotFoundAlert.defaultProps = {
+  message: 'Oops! Seems like the blog you are trying to access does not exist.'
+}
+
+export default NotFoundAlert;
