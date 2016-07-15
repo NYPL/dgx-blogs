51ca66f2518b4e24912b4a6dbd4e656b3f0ad44f
Patricio Gabriel Maseda <pmaseda@devspark.com>
2016-07-15 18:46:35 -0300

    WEB-3128 circle image in hero removed. Title repositioned.

--- src/app/components/Hero/Hero.jsx
+++ src/app/components/Hero/Hero.jsx
@@ -4,22 +4,11 @@ class Hero extends React.Component {
   constructor(props) {
     super(props);
   }
-
-  _renderPicture() {
-    if(this.props.picture) { 
-      return (
-          <img className="hero-content-picture" src={this.props.picture} />
-        );
-    }
-
-    return null;
-  }
   
   render() {
     return (
       <div className="hero">
         <div className="hero-content">
-          {this._renderPicture()}
           <p className="hero-content-serie">{this.props.type}</p>
           <h1 className="hero-content-title">{this.props.title} <span className="nypl-icon-wedge-down"></span></h1>
           <p className="hero-content-description">{this.props.description}</p>
