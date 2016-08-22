3c2d481dbd7abc946b71d0807269457cb6ba47be
Patricio Gabriel Maseda <pmaseda@devspark.com>
2016-08-22 12:12:25 -0300

    WEB-3365 harcoded svg replaced by dgx-icons component.

--- src/app/components/LoadMoreButton/LoadMoreButton.jsx
+++ src/app/components/LoadMoreButton/LoadMoreButton.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import axios from 'axios';
 import Actions from '../../actions/Actions';
 
+/* svg */
+import { DotsIcon } from 'dgx-svg-icons';
+
 class LoadMoreButton extends React.Component {
   constructor(props) {
     super(props);
@@ -29,24 +32,6 @@ class LoadMoreButton extends React.Component {
       });
   }
 
-  svgDots() {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 32 32"
-        className="svgIcon blue"
-        aria-hidden="true"
-        >
-          <title>circle.more.icon.v1</title>
-          <circle cx="16" cy="16" r="1.9029" />
-          <circle cx="24" cy="16" r="1.9029" />
-          <circle cx="8" cy="16" r="1.9029" />
-      </svg>
-      );
-  }
-
   render() {
     return (
       <div className="loadMoreButton">
@@ -54,7 +39,7 @@ class LoadMoreButton extends React.Component {
           className="loadMoreButton-btn"
           onClick={this.handleClick}
         >
-          {this.svgDots()}
+          <DotsIcon />
           <span>{this.props.postsLeft}</span>
         </button>
       </div>
