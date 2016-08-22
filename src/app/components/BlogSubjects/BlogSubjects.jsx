369407cdffd9c2d52ccf0266e8698ff62be35179
Patricio Gabriel Maseda <pmaseda@devspark.com>
2016-08-22 12:53:10 -0300

    WEB-3365 Forgotten TagIcon included as a component.

--- src/app/components/BlogSubjects/BlogSubjects.jsx
+++ src/app/components/BlogSubjects/BlogSubjects.jsx
@@ -4,6 +4,9 @@ import axios from 'axios';
 
 import Actions from '../../actions/Actions.js';
 
+/* svg */
+import { TagIcon } from 'dgx-svg-icons';
+
 class BlogSubjects extends React.Component {
   constructor(props) {
     super(props);
@@ -44,7 +47,7 @@ class BlogSubjects extends React.Component {
             to={`${this.props.appBaseUrl}subjects/${subject.id}`}
             className="tagLink"
             onClick={this.fetchSubject.bind(subject, subject.id, this)}>
-            {this.tagIcon()}
+            <TagIcon ariaHidden />
             {subject.name}
           </Link>
         </li>
@@ -52,23 +55,7 @@ class BlogSubjects extends React.Component {
     });
   }
 
-  tagIcon() {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="20"
-        viewBox="0 0 32 32"
-        className="svgIcon"
-        aria-hidden="true"
-      >
-        <title>tag.icon</title>
-        <path d="M26.45536,26.45536H16.80078L4.92843,14.58216l9.65373-9.65373,11.8732,11.87235v9.65458ZM17.87606,23.859h5.983v-5.983L14.58216,8.5992l-5.983,5.983Z" />
-      </svg>
-    );
-  }
-
-  renderContent() {
+  render() {
     /* if there is not any subject this component musnt generate any html */
     if (! this.props.subjects || this.props.subjects.length === 0) {
       return null;
@@ -85,10 +72,6 @@ class BlogSubjects extends React.Component {
         </ul>
       </div>
     );
-  }
-
-  render() {
-    return this.renderContent();
   }
 }
 
